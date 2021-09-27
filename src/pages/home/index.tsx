5dc31f4d938c2e7cbd662d0a660268d51fec2cfd
Alexandre Preczewski <alexandrep@outboxup.com>
2021-09-27 18:57:57 -0300

    create the first configurations of project

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,16 @@
 import { Header } from '@components/Header';
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const home: React.FC = () => {
   return (
-    <>
+    <Container>
       <Header title="Home" />
       <main>
         <h1>Home</h1>
       </main>
-    </>
+    </Container>
   );
 }
 
