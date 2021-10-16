51825128dbaa1a31344a8798b09b9466fe1abb4e
Alexandre Preczewski <apreczewski@gmail.com>
2021-10-16 11:57:14 -0300

    created screen contact

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -16,9 +16,12 @@ const contato: React.FC = () => {
     <Wrapper>
       <HeaderTab title="contato" />
 
-
       <h1>Multi Contato</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore.</p>
 
       <ContactBox>
         <div>
